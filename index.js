46785b2ab6f2bba3dfe6b782f7135032d2670361
wyh-code <578311720@qq.com>
2023-03-12 12:51:15 +0800

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const fs = require('fs');
+const TsxI18n = require('./lib');
+
+// 配置信息
+const config = {
+  area: 'area', // 业务场景
+  moduleName: 'moduleName', // 业务模块
+  point: '', // 功能点
+  entry: '/src/code.tsx', // 入口文件，相对项目根路径地址
+  transResult: false, // log 中是否包含翻译结果
+  importI18n: `import i18n from '@/config/i18n';`, // 
+  prettierOption: {}, // 输出文件格式化配置
+  exclude: (url) => { // 需要排除的文件
+    // console.log(url, '==url==')
+    return false;
+  },
+  getWordMap: ()=>({}), // 获取已有的文案字典
+  getLog: log => { // 获取替换日志
+    fs.writeFileSync('./log.json', JSON.stringify(log), 'utf-8')
+  },
+}
+
+const configName = process.argv.slice(2)[0];
+config.entry = `/src/${configName}.tsx`
+
+const tsxI18n = new TsxI18n(config);
+
+tsxI18n.init()
